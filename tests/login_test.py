b1e8f38c013827b47a61df9ed58774ce86d73402
Doreen DiVasta <ddivasta@gmail.com>
2022-04-05 16:03:59 -0400

    Correcting  assert so it can pass

--- tests/login_test.py
+++ tests/login_test.py
@@ -24,7 +24,7 @@ class TestLogin():
             homepage.click_welcome()
             homepage.click_logout()
             x = driver.title
-            assert x == "Orange"
+            assert x == "OrangeHRM"
 
         except AssertionError as error:
             print("Assertion error occurred")
